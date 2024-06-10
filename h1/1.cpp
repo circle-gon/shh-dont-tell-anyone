dd2506d4f9790427141d9fb55d7aeef69b5bffc8
circle-gon <97845741+circle-gon@users.noreply.github.com>
2024-06-10 22:41:59 +0000

    problem 1

--- h1/1.cpp
+++ h1/1.cpp
@@ -1,1 +1,39 @@
-// aaaaa
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    ifstream input;
+    ofstream output;
+    input.open("input.txt");
+    output.open("output.txt");
+
+    string a = "";
+    bool should = false;
+    int lines = 0;
+    int words = 0;
+    int chars = 0;
+
+    while (getline(input, a)) {
+        if (should) output << "\n";
+        should = true;
+
+        output << a;
+        lines++;
+        words++;
+        for (char l: a) {
+            if (string({l}) == " ") {
+                words++;
+            } else {
+                chars++;
+            }
+        }
+    }
+
+    cout << "Lines: " << lines << "\n";
+    cout << "Words: " << words << "\n";
+    cout << "Non-whitespace characters: " << chars << "\n";
+
+    return 0;
+}
